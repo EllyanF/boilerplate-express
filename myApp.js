d00df8a36cb71824099cbbfa24bd40ae3b9c454c
EllyanF <ellyan.fernandes@hotmail.com>
2022-12-11 13:30:04 -0300

    Displays json data on response

--- myApp.js
+++ myApp.js
@@ -4,11 +4,19 @@ console.log("Hello World");
 
 let app = express();
 
-app.use("/public", express.static(__dirname + "/public"))
+app.get("/json", (req, res) => {
+    res.json({
+        "message": "Hello json"
+    });
+})
 
+//app.use("/public", express.static(__dirname + "/public"))
+
+/*
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 })
+*/
 
 
 
