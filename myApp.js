8ecc75a41419efbc779c0c159a0f1cadf577e8ec
EllyanF <ellyan.fernandes@hotmail.com>
2022-12-11 14:19:22 -0300

    Use .env to make message upper case

--- myApp.js
+++ myApp.js
@@ -4,10 +4,17 @@ console.log("Hello World");
 
 let app = express();
 
+
 app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase")
     res.json({
-        "message": "Hello json"
+        "message": "Hello json".toUpperCase()
     });
+  else {
+    res.json({
+      "message": "Hello json"
+    });
+  }
 })
 
 //app.use("/public", express.static(__dirname + "/public"))
