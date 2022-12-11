b74f6f0d62b4b34339e909f5bead03485febe07a
EllyanF <ellyan.fernandes@hotmail.com>
2022-12-11 15:56:17 -0300

    Adds comments about each function

--- myApp.js
+++ myApp.js
@@ -1,28 +1,34 @@
-const { json } = require('body-parser');
+
 let express = require('express');
 
-//console.log("Hello World");
+/*
+--- Loggin Hello World into console log ---
+console.log("Hello World");
+*/
 
 let app = express();
 
 /*
+--- Middleware to log request's information each time someone access a route ---
 app.use(function(req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
 });
 */
 
+//--- Middleware to retrieve current time and send a JSON as response ---
 app.get("/now", function (req, res, next) {
   req.time = new Date().toString();
   next();
 },
-function (req, res) {
-  res.json({
-    "time": req.time
+  function (req, res) {
+    res.json({
+      "time": req.time
+    });
   });
-});
 
 /*
+--- Uses .env file to set message to upper case ---
 app.get("/json", (req, res) => {
   if (process.env.MESSAGE_STYLE === "uppercase")
     res.json({
@@ -36,9 +42,13 @@ app.get("/json", (req, res) => {
 })
 */
 
-//app.use("/public", express.static(__dirname + "/public"))
+/*
+--- Use an Stylesheet file to use with the HTML page ---
+app.use("/public", express.static(__dirname + "/public"))
+*/
 
 /*
+--- Send and HTML file as response ---
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 })
